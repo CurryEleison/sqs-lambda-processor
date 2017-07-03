9d883d2c117ad98ec5ca582c95d13efda0c7b9aa
Thomas Petersen <thomas@simplesite.com>
2017-07-03 09:45:05 +0000

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # sqs-lambda-processor
 Simple driver for SqsToLambdaListener. Takes arguments and runs the listener
+
+run pip install -r requirementst.txt to install dependencies 
+(preferably after initializing a virtualenv of course)
+
+Run python sqs-lambda-processor.py from the command line to get the arguments.
+
+Region defaults to eu-west-1 since I'm the only one who will ever use this :)
+
+
---
+++ sqs-lambda-processor.py
@@ -0,0 +1,19 @@
+import argparse
+from SqsToLambdaListener import SqsToLambdaListener
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--queue-url", help="Queue URL of the queue to process", required=True)
+    parser.add_argument("--function-name", help="ARN of the lambda to call", required=True)
+    parser.add_argument("--region", help="AWS region", default="eu-west-1")
+    args = parser.parse_args()
+    print args
+    queueurl = args.queue_url
+    lambdaarn = args.function_name
+    region = args.region
+    listener = SqsToLambdaListener(queueurl, lambdaarn, region_name=region)
+    listener.listen()
+
+
+if __name__ == '__main__': main()
